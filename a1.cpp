d68a793563b6794e6e8363bdaecc9bfcc9457a3a
karissegrant <106277027+karissegrant@users.noreply.github.com>
2022-06-15 01:28:25 -0700

    main for loop complete

--- a1.cpp
+++ a1.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-
 using namespace std;
 
 // takes 12 digits and return the check digit
@@ -50,73 +49,76 @@ int compute_check_digit(int arr[], int size) {
     return check_digit;
 }
 
-int main()
-{
-    int arr[12] = 
-    {
-        5,0,1,2,3,4,5,7,6,4,2,1
-    };
-    // for ()// 
-    // cin >> arr[i]
-    cout << compute_check_digit(arr, 12); // 4
-
-
-    return 0;
-}
 
 int main()
-
-int choice;
-int i;
-int number_size = 0;
-int arr[20]
-
-do
 {
-    cout << endl; 
-    cout << "---- Check Digit Calculator ----\n     ";
-    cout <<"1. EAN-8 \n" << "2. UPC-A \n" << "3. EAN-13 \n" << "4. EAN-14 \n" << "5. SSCC \n";
-    cout << "Choose your product code type: " << endl;
-    cin >> choice;
 
-    if (choice == 1)
-    {
-        cout << "Please enter the first 7 digits!\n";
-        number_size = 7;
-    }
+    int choice;
+    int i;
+    int number_size = 0;
+    int arr[17];
 
-    else if (choice == 2)
+    do
     {
-        cout << "Please enter the first 12 digits!\n";
-        number_size = 11;
-    }
+        cout << endl;
+        cout << "---- Check Digit Calculator ----\n";
+        cout << "1. EAN-8 \n" << "2. UPC-A \n" << "3. EAN-13 \n" << "4. EAN-14 \n" << "5. SSCC \n";
+        cout << "Choose your product code type: " << endl;
+        cin >> choice;
 
-    else if (choice == 3)
-    {
-        cout << "Please enter the first 12 digits!\n";
-        number_size = 12;
-    }
+        if (choice == 1)
+        {
+            cout << "Please enter the first 7 digits!\n";
+            number_size = 7;
+        }
 
-    else if (choice == 4)
-    {
-        cout << "Please enter the first 13 digits!\n";
-        number_size = 13;
-    }
+        else if (choice == 2)
+        {
+             cout << "Please enter the first 12 digits!\n";
+            number_size = 11;
+        }
 
-    else if (choice == 5)
-    {
-        cout << "Please enter the first 17 digits!\n";
-        number_size = 17;
-    }
+        else if (choice == 3)
+        {
+            cout << "Please enter the first 12 digits!\n";
+            number_size = 12;
+        }
 
-    else if (choice > 5)
-    {
+        else if (choice == 4)
+        {
+            cout << "Please enter the first 13 digits!\n";
+            number_size = 13;
+        }
+
+        else if (choice == 5)
+        {
+            cout << "Please enter the first 17 digits!\n";
+            number_size = 17;
+        }
+
+        else if (choice > 5)
+        {
         cout << "Error:" << choice << "is an invalid option. Try again!\n";
         break;
+        }
+
+        for (i = 0; i < number_size; i++)
+        {
+            cin >> arr[i];
+        }
+        cout << endl;
+        cout << "The check digit is: ";
+        cout << compute_check_digit(arr, number_size) << endl;
+        cout << "And the product code is: ";
+        for (i = 0; i < number_size; i++)
+        {
+            cout << arr[i];
+        }
+        cout << compute_check_digit(arr, number_size) << endl;
     }
 
+    while (choice <= 5);
 
+    return 0;
 
 }
-
-return 0; 
